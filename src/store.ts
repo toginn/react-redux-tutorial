0b0ccca8a609298198b146448a8c8bdf2aec2218
toginn <89682463+toginn@users.noreply.github.com>
2022-04-13 18:16:11 +0900

    Add: Counter

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
